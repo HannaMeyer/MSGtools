4576d0a1f718ecd72c903ce252138e87bb505ed3
HannaMeyer <hanna.meyer@uni-muenster.de>
2019-02-12 15:33:05 +0100

    moved functions

---
+++ R/generateFileName.R
@@ -0,0 +1,26 @@
+#' Generate msg file names
+#' @param x Raster stack containing the MSG scenes see getChannels
+#' @param date see getDate
+#' @return a vector of character strings
+#' @author Hanna Meyer
+#' @export generateFileName
+#' @examples
+#' msg_example <-getChannels(inpath=system.file("extdata/msg",package="Rainfall"),
+#' channels=c("VIS0.6","IR12.0"))
+#' generateFileName(msg_example,getDate(inpath=system.file("extdata/msg",package="Rainfall")))
+
+
+generateFileName <- function (x,date){
+
+  lut<-cbind(c("ca02p0001","ca02p0002","ca02p0003","ct01dk004","ct01dk005",
+               "ct01dk006","ct01dk007","ct01dk008","ct01dk009","ct01dk010",
+               "ct01dk011"),c(
+                 "VIS0.6","VIS0.8","NIR1.6","IR3.9","WV6.2","WV7.3","IR8.7",
+                 "IR9.7","IR10.8","IR12.0","IR13.4"))
+
+  chstring <- lut[,1][lut[,2]%in%names(x)]
+
+  return(unlist(strsplit(paste0(date,"_mt09s_",
+                                chstring,"_m1hct_1000_rg01de_003000.tif")," ")))
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MSGtools
+R functions to handle MSG SEVIRI data that have been processed with the Marburg MSG processing routines 
+
+This repository is a (currently random) collections of functions that support working with MSG SEVIRI data according to the naming,format and processing conventions of the "Marburg MSG SEVIRI Processing Routine".
+
+Install via:
+library(devtools)
+install_github("HannaMeyer/MSGtools")
